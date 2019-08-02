11db83738a83db3f8a579569448210586c3bded1
MURATA Mitsuharu <hikari.photon+mygit@gmail.com>
2019-08-02 18:02:36 +0900

    new

--- main.c
+++ main.c
@@ -6,10 +6,12 @@ int main(void){
     vector v4 = vector_new(a4, sizeof(a4)/sizeof(rational));
     rational a5[] = {{4,1},{5,1},{6,1}};
     vector v5 = vector_new(a5, sizeof(a5)/sizeof(rational));
-    print_vector(v4);
-    print_vector(v5);
 
-    print_rational(inner_product_vector(&v4, &v5));
+    print_vector(scalar_product_vector(v4, -1));
+    print_vector(v4);
+
+    print_vector(inverse_vector(v5));
+    print_vector(v5);
     
     return 0;
 }
--- vector.c
+++ vector.c
@@ -55,30 +55,49 @@ vector vector_new(rational value[], int size){
     return v;
 }
 
-/* ベクトルのスカラー倍 */
-void scalar_product_vector(int scalar, vector v){
+/* ベクトルのコピー */
+vector copy_vector(struct vector v){
+    int i;
+    vector tmp_v;
+    tmp_v = zero_vector_new(v.size);
+    for(i=0; i<v.size; i++){
+        tmp_v.vector[i] = v.vector[i];
+    }
+    return tmp_v;
+}
+
+/* ベクトルのスカラー倍(破壊) */
+void _scalar_product_vector(vector v, int scalar){
     int size = v.size;
     _vector *p = v.vector;
     int i;
-    for(i=0;i<size;i++){
+    for(i=0;i<size;i++)
         p[i].numerator = scalar * (p[i].numerator);
-    }
 }
 
-/* 逆ベクトル */
-void inverse_vector(vector v){
-    scalar_product_vector(-1, v);
+/* 逆ベクトル(破壊) */
+void _inverse_vector(vector v){
+    _scalar_product_vector(v, -1);
+}
+
+/* ベクトルのスカラー倍(非破壊) */
+vector scalar_product_vector(struct vector v, int scalar){
+    vector vec;
+    vec = copy_vector(v);
+    _scalar_product_vector(vec, scalar);
+    return vec;
+}
+
+/* 逆ベクトル(非破壊) */
+vector inverse_vector(vector v){
+    return scalar_product_vector(v, -1);
 }
 
 /* 内積 */
 rational inner_product_vector(struct vector *v0, struct vector *v1){
     int i;
     rational sum = {0, 1};
-    rational *r0 = v0 -> vector;
-    rational *r1 = v1 -> vector;
-    int size = v1 -> size;
-    for(i=0; i<size; i++){
-        sum = rplusr(sum, rproductr(r0[i], r1[i]));
-    }
+    for(i=0; i<v1 -> size; i++)
+        sum = rplusr(sum, rproductr((v0 -> vector)[i], (v1 -> vector)[i]));
     return sum;
 }
